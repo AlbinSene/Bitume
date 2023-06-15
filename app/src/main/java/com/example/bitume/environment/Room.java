d20df6abc776078c1c4148884733f769beb9d26e
Sene <albin.sene@gmail.com>
2023-06-15 15:19:45 +0200

    correction room

---
+++ app/src/main/java/com/example/bitume/environment/Room.java
@@ -0,0 +1,15 @@
+package com.example.bitume.environment;
+
+public class Room extends Environment {
+
+    public Room()  {
+        this.ressLow = 2;
+        this.ressHigh = 40;
+
+        this.nr0 = 0;
+        this.nr1 = 60;
+        this.nr2 = 30;
+        this.nr3 = 10;
+    }
+
+}
